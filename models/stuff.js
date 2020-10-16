640dca7f45cc28320ba0344e3875edb0759f5166
dyoder838 <70074229+dyoder838@users.noreply.github.com>
2020-10-16 13:40:32 -0700

    initialized file structure

---
+++ models/stuff.js
@@ -0,0 +1,8 @@
+module.exports = function(sequelize, DataTypes) {
+    var Stuff = sequelize.define("Stuff", {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING
+    });
+    return Stuff;
+  };
+  
