ea2cf3ed5aa2afdadfe9927d5741f8034e4b58a6
Jae McLain <jae.mclain@gmail.com>
2020-10-20 16:08:59 -0700

    schema.sql parent.js student.js teacher.js properties added

--- server.js
+++ server.js
@@ -4,17 +4,45 @@
 
 // Dependencies
 // =============================================================
-var express = require("express");
-var db = require("./models")
+// var express = require("express");
+// var db = require("./models")
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+// var app = express();
+// var PORT = process.env.PORT || 8080;
 
 
 // Starts the server to begin listening
 // =============================================================
+
+// =============================================================
+
+
+var express = require('express');
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+// var allRoutes = require('./controllers');
+
+// Requiring our models for syncing
+var db = require('./models');
+
+// Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// Static directory
+// app.use(express.static('public'));
+
+// var exphbs = require('express-handlebars');
+// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// app.set('view engine', 'handlebars');
+
+app.get('/', function(req,res){
+    res.send("home page")
+})
 
 db.sequelize.sync().then(function(){
     app.listen(PORT, function() {
