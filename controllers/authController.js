55e50c2ff237f5660af0afeba3fe260b551aec04
Jae McLain <jae.mclain@gmail.com>
2020-10-22 12:51:36 -0700

    added authcontroller file and routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const router= express.Router();
 const db = require('../models');
 const bcrypt = require('bcrypt');
 
+// Parent Signup
 router.post('/signup', (req, res) => {
     db.Parent.create({
         first_name: req.body.parentFirst,
@@ -19,13 +20,35 @@ router.post('/signup', (req, res) => {
             email: newUser.email,
             id: newUser.id
         }
-        res.redirect("/parentpage")
+        res.redirect("/parent")
     }).catch(err => {
         console.log(err);
         res.status(500).send("server error")
     })
 })
 
+
+// Teacher Signup
+router.post('/signup', (req, res) => {
+    db.Teacher.create({
+        first_name: req.body.teacherFirst,
+        last_name: req.body.teacherLast,
+        email: req.body.teacherEmail,
+        password: req.body.teacherPassword,
+    }).then(newUser => {
+        req.session.user = {
+            email: newUser.email,
+            id: newUser.id
+        }
+        res.redirect("/teacher")
+    }).catch(err => {
+        console.log(err);
+        res.status(500).send("server error")
+    })
+})
+
+
+// Parent Login
 router.post('/login', (req, res) => {
     db.Parent.findOne({
         where: { email: req.body.parentEmail }
@@ -40,7 +63,7 @@ router.post('/login', (req, res) => {
                 email: user.email,
                 id: user.id
             }
-            return res.redirect("/")
+            return res.redirect("/parent")
         }
         else {
             req.session.destroy();
@@ -48,3 +71,44 @@ router.post('/login', (req, res) => {
         }
     })
 })
+
+
+// Teacher Login
+router.post('/login', (req, res) => {
+    db.Teacher.findOne({
+        where: { email: req.body.teacherEmail }
+    }).then(user => {
+        //check if user entered password matches db password
+        if (!user) {
+            req.session.destroy();
+            return res.status(401).send('incorrect email or password')
+
+        } else if (bcrypt.compareSync(req.body.password, user.password)) {
+            req.session.user = {
+                email: user.email,
+                id: user.id
+            }
+            return res.redirect("/teacher")
+        }
+        else {
+            req.session.destroy();
+            return res.status(401).send('incorrect email or password')
+        }
+    })
+})
+
+
+// Logout 
+router.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.send('logged out')
+})
+
+// Session 
+router.get("/sessiondata", (req, res) => {
+    res.json(req.session)
+})
+
+
+
+module.exports = router;
