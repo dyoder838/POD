625fd3bde583218441a228397cd191f7dcfb9fa1
Maged Abdelsalam <dagadego@gmail.com>
2020-10-25 16:24:46 -0700

    Fix teacher-parent mixup

--- controllers/podController.js
+++ controllers/podController.js
@@ -21,7 +21,7 @@ router.get("/parent/view",(req,res)=>{
     }).then(pods=>{
         const podsJson=pods.map(pod=>pod.toJSON());
         console.log("podsJson from teacher view api call/ whats in it? " ,podsJson)
-        res.render("parent",{teacher: podsJson});
+        res.render("teacher",{teacher: podsJson});
     })
 })
 module.exports = router;
